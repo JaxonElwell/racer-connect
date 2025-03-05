85cc1be4f58ea76d44cfb6cb6623a88c3ef2455d
BuildTools <unconfigured@null.spigotmc.org>
2025-03-04 23:25:11 -0600

    added endpoints for get

--- racer-connect/server/index.js
+++ racer-connect/server/index.js
@@ -16,9 +16,39 @@ const db = new sqlite3.Database('./devdb.sqlite', (err) => {
     }
 });
 
-// example API endpoint
-app.get('/api/jobs', (req, res) => {
-    db.all('SELECT * FROM jobs', [], (err, rows) => {
+// API endpoints
+app.get('/api/EventAttendees', (req, res) => {
+    db.all('SELECT * FROM StudentOrganizations', [], (err, rows) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({ data: rows });
+    });
+});
+
+app.get('/api/Events', (req, res) => {
+    db.all('SELECT * FROM StudentOrganizations', [], (err, rows) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({ data: rows });
+    });
+});
+
+app.get('/api/StudentOrganizations', (req, res) => {
+    db.all('SELECT * FROM StudentOrganizations', [], (err, rows) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({ data: rows });
+    });
+});
+
+app.get('/api/Users', (req, res) => {
+    db.all('SELECT * FROM StudentOrganizations', [], (err, rows) => {
         if(err) {
             res.status(400).json({ error: err.message });
             return;
@@ -30,5 +60,5 @@ app.get('/api/jobs', (req, res) => {
 // start the server
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log('Server running on http://localhost:${PORT}');
+    console.log('Server running on http://localhost:5000');
 });
